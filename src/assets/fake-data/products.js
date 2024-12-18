07c8ff2ae1958c91211a010dafb8dd72c691d067
amina aksouh <amina10aks@gmail.com>
2024-12-18 12:05:15 +0100

    update photo

--- src/assets/fake-data/products.js
+++ src/assets/fake-data/products.js
@@ -3,6 +3,14 @@ import product_01_image_01 from "../images/product_01.jpg";
 import product_01_image_02 from "../images/product_01.1.jpg";
 import product_01_image_03 from "../images/product_01.3.jpg";
 
+import product_05_image_01 from "../images/product_04.jpg";
+import product_05_image_02 from "../images/product_08.jpg";
+import product_05_image_03 from "../images/product_09.jpg";
+
+import product_05_image_04 from "../images/product_07.jpg";
+import product_05_image_06 from "../images/product_06.jpg";
+
+
 import product_02_image_01 from "../images/product_2.1.jpg";
 import product_02_image_02 from "../images/product_2.2.jpg";
 import product_02_image_03 from "../images/product_2.3.jpg";
@@ -13,17 +21,20 @@ import product_03_image_03 from "../images/product_3.3.jpg";
 
 import product_04_image_01 from "../images/product_4.1.jpg";
 import product_04_image_02 from "../images/product_4.2.jpg";
-import product_04_image_03 from "../images/product_4.3.png";
+//import product_04_image_03 from "../images/product_4.3.png";
 
-import product_05_image_01 from "../images/product_04.jpg";
-import product_05_image_02 from "../images/product_08.jpg";
-import product_05_image_03 from "../images/product_09.jpg";
 
-import product_06_image_01 from "../images/bread(1).png";
-import product_06_image_02 from "../images/bread(2).png";
-import product_06_image_03 from "../images/bread(3).png";
+
+import product_06_image_01 from "../images/product_6.1.jpg";
+import product_06_image_02 from "../images/product_6.2.jpg";
+import product_06_image_03 from "../images/product_6.3.jpg";
+import product_06_image_04 from "../images/product_6.4.jpg";
+import product_06_image_05 from "../images/product_6.5.jpg";
+import product_06_image_06 from "../images/product_6.6.jpg";
 
 const products = [
+ 
+
   {
     id: "01",
     title: "Chicken Burger",
@@ -66,8 +77,8 @@ const products = [
     price: 110.0,
     image01: product_04_image_01,
     image02: product_04_image_02,
-    image03: product_04_image_03,
-    category: "Pizza",
+    image03: product_03_image_03,
+    category: "Tarte",
 
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
   },
@@ -87,8 +98,8 @@ const products = [
     id: "06",
     title: "Royal Cheese Burger",
     price: 24.0,
-    image01: product_01_image_01,
-    image02: product_01_image_02,
+    image01: product_01_image_02,
+    image02: product_01_image_01,
     image03: product_01_image_03,
     category: "Cake",
 
@@ -125,7 +136,7 @@ const products = [
     price: 110.0,
     image01: product_04_image_02,
     image02: product_04_image_01,
-    image03: product_04_image_03,
+    image03: product_03_image_02,
     category: "Tarte",
 
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
@@ -173,11 +184,122 @@ const products = [
     price: 35.0,
     image01: product_06_image_03,
     image02: product_06_image_02,
-    image03: product_06_image_03,
+    image03: product_06_image_01,
     category: "Viennoiserie",
 
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
   },
+  {
+    id: "14",
+    title: "Chicken Burger",
+    price: 24.0,
+    image01: product_05_image_04,
+    image02: product_01_image_02,
+    image03: product_01_image_03,
+    category: "Cake",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque. ",
+  },
+
+
+
+  {
+    id: "15",
+    title: "Chicken ",
+    price: 24.0,
+    image01: product_01_image_03,
+    image02: product_01_image_02,
+    image03: product_01_image_01,
+    category: "Cake",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque. ",
+  },
+
+  {
+    id: "16",
+    title: "Chicken t ",
+    price: 24.0,
+    image01: product_05_image_06,
+    image02: product_01_image_02,
+    image03: product_01_image_01,
+    category: "Cake",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque. ",
+  },
+
+  {
+    id: "17",
+    title: "Chicken B",
+    price: 24.0,
+    image01: product_05_image_03,
+    image02: product_01_image_02,
+    image03: product_01_image_03,
+    category: "Cake",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque. ",
+  },
+
+  {
+    id: "18",
+    title: "Vegetarian Pizza",
+    price: 115.0,
+    image01: product_03_image_03,
+    image02: product_02_image_01,
+    image03: product_02_image_03,
+    category: "Tarte",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "19",
+    title: "Vegetarian ",
+    price: 115.0,
+    image01: product_02_image_03,
+    image02: product_02_image_02,
+    image03: product_02_image_01,
+    category: "Tarte",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "20",
+    title: "Vegetarian Pizza",
+    price: 115.0,
+    image01: product_06_image_04,
+    image02: product_06_image_05,
+    image03: product_06_image_06,
+    category: "Viennoiserie",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "21",
+    title: "Vegetarian Pizza",
+    price: 115.0,
+    image01: product_06_image_05,
+    image02: product_06_image_04,
+    image03: product_06_image_06,
+    category: "Viennoiserie",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "22",
+    title: "Vegetarian Pizza",
+    price: 115.0,
+    image01: product_06_image_06,
+    image02: product_06_image_05,
+    image03: product_06_image_04,
+    category: "Viennoiserie",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  
 ];
 
 export default products;
